94a767918c6d36bab0374fbc8f532e3bdac2cdde
Loc Tran <tranphuocloc070699@gmail.com>
2024-12-03 18:12:42 +0700

    feat: office

--- components/common/AuthContainer.tsx
+++ components/common/AuthContainer.tsx
@@ -12,12 +12,35 @@ import NextImg from 'next/image';
 import useCustomState from "@/hooks/use-state";
 import Typography from "@/components/common/Typography";
 import background from "@/public/assets/images/login-background.png";
+import mobileBackgroundTop from "@/public/assets/images/mobile-bg-top.svg";
+import mobileBackgroundBottom from "@/public/assets/images/mobile-bg-bot.svg";
 import logo from "@/public/assets/images/Logo.svg"
 import Input from "./Input";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {FieldValues, useForm} from "react-hook-form";
 
 interface Props {
   type: "login" | "signup";
 }
+
+const schema = yup.object({
+  email: yup
+  .string()
+  .email("Email không hợp lệ")
+  .required("Email là bắt buộc"),
+  password: yup
+  .string()
+  .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
+  .required("Mật khẩu là bắt buộc"),
+  full_name: yup
+  .string()
+  .required("Tên đầy đủ là bắt buộc"),
+  password_confirmation: yup
+  .string()
+  .oneOf([yup.ref('password')], "Mật khẩu xác nhận không khớp")
+  .required("Mật khẩu xác nhận là bắt buộc")
+});
 
 const AuthContainer = ({type}: Props) => {
   const {toast} = useToast();
@@ -37,6 +60,13 @@ const AuthContainer = ({type}: Props) => {
     },
   };
 
+
+  const {control, handleSubmit} = useForm({
+    defaultValues: {
+      email: '',
+    },
+  });
+
   const {fields, updateFields} = useCustomState<ISignUpLoginForm>({
     email: "",
     full_name: "",
@@ -44,38 +74,62 @@ const AuthContainer = ({type}: Props) => {
     password_confirmation: "",
   });
 
-  async function onSubmit() {
-    const userService = new UserService();
+  // async function onSubmit() {
+  //   const userService = new UserService();
+  //
+  //   let response;
+  //   if (type === "signup") {
+  //     response = await userService.signup(fields);
+  //   }
+  //   if (type === "login") {
+  //     response = await userService.login(fields);
+  //   }
+  //   console.log({response});
+  //   if (response?.success) {
+  //     toast({variant: "default", title: "Đăng ký thành công"});
+  //   } else {
+  //     toast({
+  //       variant: "destructive",
+  //       title: "Có lỗi xảy ra",
+  //     });
+  //   }
+  // }
 
-    let response;
-    if (type === "signup") {
-      response = await userService.signup(fields);
-    }
-    if (type === "login") {
-      response = await userService.login(fields);
-    }
-    console.log({response});
-    if (response?.success) {
-      toast({variant: "default", title: "Đăng ký thành công"});
-    } else {
-      toast({
-        variant: "destructive",
-        title: "Có lỗi xảy ra",
-      });
-    }
+  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+  //   console.log({e, fields})
+  // }
+
+  function onSubmit(data: any) {
+    console.log({data})
   }
 
   return (
-      <div
+      <form
+          onSubmit={handleSubmit(data => onSubmit(data))}
           className="relative w-full min-h-screen flex justify-center items-center p-5 md:bg-sh-background bg-sh-secondary-300">
-        {/* Background image using NextImg */}
-
         <NextImg
             className="absolute inset-0 object-cover w-full h-full md:block hidden"
             src={background}
             quality={100}
             alt="background"
             layout="fill"
+        />
+        <NextImg
+            className="absolute top-0 right-0 md:hidden"
+            src={mobileBackgroundTop}
+            quality={100}
+            alt="mobile background top"
+            width={180}
+            height={45}
+        />
+        <NextImg
+            className="absolute bottom-0 left-0 md:hidden"
+            src={mobileBackgroundBottom}
+            quality={100}
+            alt="mobile background bot"
+            width={180}
+            height={45}
         />
         <NextImg
             className="absolute md:left-[40px] md:top-6 left-4 top-4 object-cover md:w-[180px] w-[100px] h-auto"
@@ -86,13 +140,38 @@ const AuthContainer = ({type}: Props) => {
             height={45}
         />
 
-        {/* Content */}
+
         <div
             className="absolute left-[50%] translate-y-[-50%] top-[50%] translate-x-[-50%] z-10 bg-sh-secondary-300 rounded-lg md:p-6 p-4 flex flex-col items-center justify-center ">
           <Typography.H2>{data[type].title}</Typography.H2>
-          <Input placeholder={"Nhập email"} label={"Email"}/>
+          <Input icon={{name: "email", showIcon: true}}
+                 input={{
+                   placeholder: "Nhập email",
+                   value: fields.email,
+                   type: "text",
+                   onChange: (e) => updateFields({email: e.target.value}),
+                 }}
+                 label={{
+                   showLabel: true,
+                   label: "Email",
+                 }}
+                 control={control}
+                 name='email'
+                 rules={{required: {value: true, message: 'Required'}}}
+          />
+          {/*<Input icon={{name: "lock", showIcon: true}}*/}
+          {/*       input={{*/}
+          {/*         placeholder: "Nhập password",*/}
+          {/*         value: fields.password,*/}
+          {/*         type: "password",*/}
+          {/*         onChange: (e) => updateFields({password: e.target.value})*/}
+          {/*       }}*/}
+          {/*       label={{showLabel: true, label: "Password"}}*/}
+          {/*       variant={"primary"}/>*/}
+
+          <button type={"submit"}>Submit</button>
         </div>
-      </div>
+      </form>
 
   );
 };
--- components/common/Icon.tsx
+++ components/common/Icon.tsx
@@ -8,11 +8,11 @@ import {
   Unlink,
   Heart,
   CircleX,
-  TextSearch,
+  TextSearch, Mail, LockKeyhole, Eye, EyeOff, TriangleAlert,
 } from "lucide-react";
 
-// Define icons with `as const` to make keys readonly
-const icons = {
+
+export const icons = {
   plus: <Plus/>,
   cloudUpload: <CloudUpload/>,
   check: <Check/>,
@@ -22,23 +22,30 @@ const icons = {
   unLink: <Unlink/>,
   emptyHeart: <Heart/>,
   emptyData: <TextSearch/>,
+  email: <Mail/>,
+  lock: <LockKeyhole/>,
+  eye: <Eye/>,
+  eyeOff: <EyeOff/>,
+  warning: <TriangleAlert/>
 } as const;
 
 interface Props {
   name: keyof typeof icons | string;
   className?: string;
   size?: number;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-const Icon = ({name, className, size}: Props) => {
+const Icon = ({name, className, size, onClick}: Props) => {
   const isIcon = name in icons;
 
   if (isIcon) {
     const icon = icons[name as keyof typeof icons];
-    return React.cloneElement(icon as React.ReactElement, {className, size});
+    return React.cloneElement(icon as React.ReactElement, {className, size, onClick});
   }
 
-  return <img src={name} alt={name} className={className} style={{width: size, height: size}}/>;
+  return <img onClick={onClick} src={name} alt={name} className={className}
+              style={{width: size, height: size}}/>;
 };
 
 export default Icon;
